fc72f8b8d4461cf18be3019341d52527772cd3e9
Liam Callaghan <ljcallaghan64@gmail.com>
2021-07-17 17:46:08 +0100

    home component

---
+++ src/components/common/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Home extends React.Component {
+
+  render() {
+    return (
+      <section>
+        <h1>hello</h1>
+      </section>
+    )
+  }
+}
+
+export default Home
