3fd44cf368d6391fa21a5e6517f1349d11f84a68
Liam Callaghan <ljcallaghan64@gmail.com>
2021-07-19 21:44:18 +0100

    infobar added

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import InfoBar from './InfoBar'
 import avip from '/Users/liam/Development/avip/src/images/AVIP.png'
 import oneBrackets from '/Users/liam/Development/avip/src/images/-1.png'
 
@@ -8,7 +9,9 @@ class Home extends React.Component {
     return (
       <section>
         <img className='logo' src={oneBrackets} alt='logo' />
-        <img className='banner' src={avip} alt='banner' />
+        <div className='banner'><img className='mainLogo' src={avip} alt='banner' /></div>
+        <h1>Coming Soon</h1>
+        <InfoBar />
       </section>
     )
   }
---
+++ src/components/common/InfoBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import instagramIcon from '/Users/liam/Development/avip/src/images/instagram-icon.png'
+
+const InfoBar = () => {
+  return (
+    <section className='infoBar'>
+      <img className='socialIcon' src={instagramIcon} />
+    </section>
+  )
+}
+
+export default InfoBar
