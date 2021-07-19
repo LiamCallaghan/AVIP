acef46eb3acb471a43a5de5246a6ca7f00f707dc
Liam Callaghan <ljcallaghan64@gmail.com>
2021-07-19 20:52:52 +0100

    tab styling done, images displaying on page

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import avip from '/Users/liam/Development/avip/src/images/AVIP.png'
+import oneBrackets from '/Users/liam/Development/avip/src/images/-1.png'
 
 class Home extends React.Component {
 
   render() {
     return (
       <section>
+        <img className='logo' src={oneBrackets} alt='logo' />
         <img className='banner' src={avip} alt='banner' />
       </section>
     )
