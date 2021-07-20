304a3622d011b60e0cc0340d6c8357eeebe1e5d1
Liam Callaghan <ljcallaghan64@gmail.com>
2021-07-21 00:10:05 +0100

    infobar color

--- src/components/common/InfoBar.js
+++ src/components/common/InfoBar.js
@@ -3,8 +3,8 @@ import React from 'react'
 const InfoBar = () => {
   return (
     <section className='infoBar'>
-      <img className='socialIcon' src='https://i.imgur.com/hdnmUWo.png' />
-      <img className='socialIcon' src='https://i.imgur.com/HssvT9b.png' />
+      <button><img className='socialIcon' src='https://i.imgur.com/hdnmUWo.png' /></button>
+      <button><img className='socialIcon' src='https://i.imgur.com/HssvT9b.png' /></button>
     </section>
   )
 }
