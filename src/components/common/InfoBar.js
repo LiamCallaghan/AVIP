c401fe17211e8f49a74eda1500c44904d2a5e7d2
Liam Callaghan <ljcallaghan64@gmail.com>
2021-07-22 21:45:12 +0100

    footer input

--- src/components/common/InfoBar.js
+++ src/components/common/InfoBar.js
@@ -3,8 +3,13 @@ import React from 'react'
 const InfoBar = () => {
   return (
     <section className='infoBar'>
-      <button><img className='socialIcon' src='https://i.imgur.com/hdnmUWo.png' /></button>
-      <button><img className='socialIcon' src='https://i.imgur.com/HssvT9b.png' /></button>
+      <div className='emailInput'>
+        <input placeholder='@'></input>
+      </div>
+      <div className='socialBox'>
+        <button><img className='socialIcon' src='https://i.imgur.com/hdnmUWo.png' /></button>
+        <button><img className='socialIcon' src='https://i.imgur.com/HssvT9b.png' /></button>
+      </div>
     </section>
   )
 }
